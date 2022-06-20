66bfa0ce4efb66d3d6ae49dbe43e0fd49c297d1e
Ariful Islam Arif <devboyarif@gmail.com>
2022-06-20 11:51:03 +0600

    webpack file update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/admin/js')
+mix.js('resources/js/app.js', 'public/admin/js/compiled')
     .vue(3)
     .postCss('resources/css/app.css', 'public/admin/css', [
         //
