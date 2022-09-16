5dce03a54fccd2cfa01313d473425e12e8f8b2c7
Ariful Islam Arif <devboyarif@gmail.com>
2022-09-16 11:01:14 +0600

    remove unused files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,50 +21,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // For Packaging
-        $this->packagingVersion();
-
-        // For Development
-        // $this->developmentVersion();
-    }
-
-    private function packagingVersion()
-    {
         $this->call([
             MasterSeeder::class,
             ThemeSeeder::class,
             CountrySeeder::class,
             FeatureSeeder::class,
             SettingSeeder::class,
-            SeoSeeder::class,
-            CmsSeeder::class,
-        ]);
-    }
-
-    private function developmentVersion()
-    {
-        $this->call([
-            TeamSizeSeeder::class,
-            PlanSeeder::class,
-            AdminSeeder::class,
-            ThemeSeeder::class,
-            CountrySeeder::class,
-            CompanySeeder::class,
-            // LeaveTypeSeeder::class,
-            TeamSeeder::class,
-            EmployeeSeeder::class,
-            CalendarSeeder::class,
-            LeaveRequestSeeder::class,
-            HolidayRequestSeeder::class,
-            PostSeeder::class,
-            FaqSeeder::class,
-            FeatureSeeder::class,
-            TestimonialSeeder::class,
-            LanguageSeeder::class,
-            CurrencySeeder::class,
-            OrderSeeder::class,
-            SettingSeeder::class,
-            ContactMessageSeeder::class,
             SeoSeeder::class,
             CmsSeeder::class,
         ]);
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,3 @@ include(base_path('routes/role/admin.php'));
 include(base_path('routes/role/company.php'));
 include(base_path('routes/role/employee.php'));
 include(base_path('routes/payment.php'));
-
-include(base_path('routes/test.php'));
